245399bc449763ef31ca0ba201645eb44806bddf
Santiorozco7 <113224702+Santiorozco7@users.noreply.github.com>
2024-02-06 14:53:58 -0500

    Change in session logging. (#10)

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormControl, Validators, FormGroup } from '@angular/forms';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-login',
@@ -9,40 +10,97 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  err=false;
+  err:boolean =false;
+  role:string = '';
 
-  constructor(private FB: FormBuilder, private service: AuthService, private router: Router) {}
+  docTypeselect: any[] = [
+    { id: 0, name: 'CC' },
+    { id: 1, name: 'TI' },
+    { id: 2, name: 'CE' },
+    { id: 3, name: 'NIT' },
+  ];
 
-  formUser = this.FB.group({
-    'documento': ['', Validators.required],
-    'password': ['', Validators.required]
+  constructor(private formBuilder: FormBuilder, private service: AuthService, private router: Router) {}
+
+  userForm = this.formBuilder.group({
+    docType: ['', Validators.required],
+    documento: ['', Validators.required],
+    password: ['', Validators.required],
   });
 
   get documento() {
-    return this.formUser.get('documento') as FormControl;
+    return this.userForm.get('documento') as FormControl;
   }
 
   get password() {
-    return this.formUser.get('password') as FormControl;
+    return this.userForm.get('password') as FormControl;
+  }
+
+  roleSelect(role:string):void {
+    console.log('Valor seleccionado:', role);
+    switch (role) {
+      case '0':
+        this.role = role;
+        console.log('Ingresa un agriculto');
+        break;
+      
+      case '1':
+        this.role = role;
+        console.log('Ingresa un colaborador');
+        break;
+
+      case '3':
+        this.role = role;
+        console.log('Ingresa un asociado');
+        break;
+
+      default:
+        console.log('Aun no hay seleccion');
+        break;
+    }
   }
 
   login() {
-    const docNumber = this.documento.value;
-    const password = this.password.value;
-    
-    this.service.login(docNumber, password).subscribe(credenciales =>{
-      console.log(credenciales);
-      if(credenciales['state'] === 'Ok') {
-        if (credenciales['role'] === "0") {
-          this.router.navigate(['/agricultor']); // Redirigir al módulo de agricultor.
-        } else if (credenciales['role'] === "1") {
-           this.router.navigate(['/asociacion']); // Redirigir al módulo de asociacion.
-        } else if (credenciales['role'] === "2") {
-           this.router.navigate(['/colaborador']); // Redirigir al módulo de colaborador.
+    if (this.userForm.valid) {
+      const formData = this.userForm.value;
+
+      const docType: string = formData.docType || "";
+      const docNumber: string = formData.documento || "";
+      const password: string = formData.password || "";
+      console.log(docType,' ',docNumber,' ',password,' ',this.role);
+      this.service.login(docType, docNumber, password, this.role).subscribe(credenciales =>{
+        if(credenciales['state'] === 'Ok') {
+          console.log('Ingreso ',credenciales);
+          if (this.role) {
+            console.log("Con rol");
+            if (this.role === "0" && credenciales.roles.isFarmer) {
+              console.log("entro como agricultor por medio la seleccion del rol ");
+              this.router.navigate(['/agricultor']); // Redirigir al módulo de agricultor.
+            } else if (this.role === "3" && credenciales.roles.isAdmin) {
+              console.log("entro como asociado por medio la seleccion del rol ");
+               this.router.navigate(['/asociacion']); // Redirigir al módulo de asociacion.
+            } //else if (credenciales['role'] === "1" && credenciales.roles.isCollab) {
+            //    this.router.navigate(['/colaborador']); // Redirigir al módulo de colaborador.
+            // }
+          }else if (!this.role) {
+            console.log("Sin rol");
+            if (credenciales['role'] === "0" && credenciales.roles.isFarmer) {
+              console.log("entro como agricultor por medio del rol inicial ");
+              this.router.navigate(['/agricultor']); // Redirigir al módulo de agricultor.
+            } else if (credenciales['role'] === "1" && credenciales.roles.isAdmin) {
+              console.log("entro como asociado por medio del rol inicial ");
+               this.router.navigate(['/asociacion']); // Redirigir al módulo de asociacion.
+            } //else if (credenciales['role'] === "1" && credenciales.roles.isCollab) {
+            //    this.router.navigate(['/colaborador']); // Redirigir al módulo de colaborador.
+            // }
+            
+
+          }
+        } else if (credenciales['state'] === 'Fail'){
+          console.log('No ingreso ',credenciales);
+          this.err = true;
         }
-      } else if (credenciales['state'] === 'Fail'){
-        this.err = true;
-      }
-    });
+      });
+    }
   }
 }
